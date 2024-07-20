d15b24efe8ea04a2fb4ecf92b87f1a9ec827d27d
jhpark0323 <brianwogus@naver.com>
2024-07-21 00:21:32 +0900

    jpa

---
+++ jpa/demo/src/main/java/jpabasic/reserve/start/JpabeginApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.reserve.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpabeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpabeginApplication.class, args);
+	}
+
+}
---
+++ jpa/demo/src/test/java/jpabasic/reserve/start/JpabeginApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.reserve.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpabeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
